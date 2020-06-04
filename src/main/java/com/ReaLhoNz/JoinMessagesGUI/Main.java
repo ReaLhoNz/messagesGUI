8e0720de0300270b777fd8020476c16d93bc240d
1vomarek1 <keramnej@gmail.com>
2020-06-04 19:28:59 +0200

    improved package structure, EventPriorities

--- src/main/java/com/ReaLhoNz/JoinMessagesGUI/Main.java
+++ src/main/java/com/ReaLhoNz/JoinMessagesGUI/Main.java
@@ -1,15 +1,16 @@
-package joinmessages.gui.joinmessages.gui.JoinMessages;
+package com.ReaLhoNz.JoinMessagesGUI;
 
+import com.ReaLhoNz.JoinMessagesGUI.Events.CustomMessages;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class GUI extends JavaPlugin {
+public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         getServer().getConsoleSender().sendMessage(ChatColor.translateAlternateColorCodes('&', "&aOnline"));
-        getServer().getPluginManager().registerEvents(new JoinMessages(),this);
+        getServer().getPluginManager().registerEvents(new CustomMessages(),this);
 
     }
 
