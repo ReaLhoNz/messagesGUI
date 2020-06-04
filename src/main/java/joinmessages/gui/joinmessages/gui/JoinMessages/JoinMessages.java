a709d445c022a24b3afed24ed6c927fa9f643d65
ReaLhoNz <realhonz@gmail.com>
2020-06-04 19:13:17 +0200

    +Join message and Death message

--- src/main/java/joinmessages/gui/joinmessages/gui/JoinMessages/JoinMessages.java
+++ src/main/java/joinmessages/gui/joinmessages/gui/JoinMessages/JoinMessages.java
@@ -6,7 +6,10 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
 public class JoinMessages implements Listener{
 
 
@@ -16,13 +19,40 @@ public class JoinMessages implements Listener{
          event.setJoinMessage(null);
          Player player = event.getPlayer();
 
-            for(Player P :Bukkit.getOnlinePlayers()){
+         for(Player P :Bukkit.getOnlinePlayers()){
 
 
-    P.sendMessage(ChatColor.translateAlternateColorCodes('&', PlaceholderAPI.setPlaceholders(player,"&a+ %player_displayname%")));
+            P.sendMessage(ChatColor.translateAlternateColorCodes('&', PlaceholderAPI.setPlaceholders(player,"&a&l+ %player_displayname%")));
 
-                    }
-                }
-            }
+
+         }
+     }
+
+    @EventHandler
+    public void LeaveMessages(PlayerQuitEvent event){
+        event.setQuitMessage(null);
+        Player player = event.getPlayer();
+
+        for(Player P :Bukkit.getOnlinePlayers()){
+
+
+            P.sendMessage(ChatColor.translateAlternateColorCodes('&', PlaceholderAPI.setPlaceholders(player,"&c&l- %player_displayname%")));
+
+
+        }
+     }
+
+    @EventHandler
+    public void DeathMessages(PlayerDeathEvent event){
+        event.setDeathMessage(null);
+        Player player = event.getEntity();
+
+        for(Player P :Bukkit.getOnlinePlayers()){
+
+
+            P.sendMessage(ChatColor.translateAlternateColorCodes('&', PlaceholderAPI.setPlaceholders(player,"&c%player_displayname% Umřel")));
+        }
+     }
+}
 
 
